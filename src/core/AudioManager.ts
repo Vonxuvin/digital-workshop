d47c79cf1a3dc6032a349f85d634d59ca92f525c
Vonxuvin <Vonxuvin@users.noreply.github.com>
2026-05-16 21:21:06 +0000

    fix(audio): 修复GAME_OVER和LEVEL_COMPLETED事件导致音效重复播放的BUG
    移除 AudioManager 中对 GAME_OVER 和 LEVEL_COMPLETED 事件
    的监听。SceneManager.failGame() 和 completeLevel() 内部
    已经调用 audioManager.play() 播放对应音效，AudioManager
    再监听这两个事件会导致音效被播放两次。
    
    同时 GameEventRouter 也监听这两个事件分别调用
    sceneManager.failGame() 和 completeLevel()，形成
    事件 -> Router -> SceneManager -> play 音效
    的正确链路，AudioManager 不应重复监听。

--- src/core/AudioManager.ts
+++ src/core/AudioManager.ts
@@ -39,8 +39,6 @@ export class AudioManager {
     eventBus.on(GameEvents.BLOCK_DROPPED, () => this.playSfx('spawn'));
     eventBus.on(GameEvents.BLOCK_MERGED, (data: any) => this.playMergeSound(data.newValue));
     eventBus.on(GameEvents.SCORE_UPDATED, (data: any) => this.playComboSound(data.chainCount));
-    eventBus.on(GameEvents.GAME_OVER, () => this.playSfx('gameOver'));
-    eventBus.on(GameEvents.LEVEL_COMPLETED, () => this.playSfx('levelComplete'));
     eventBus.on(GameEvents.PROPS_USED, (data: any) => this.playPropSound(data.type));
     eventBus.on(GameEvents.UI_BUTTON_CLICK, () => this.playSfx('click'));
   }
